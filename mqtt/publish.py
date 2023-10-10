6078c63e59cdfd83c13f234e6fa7582507574616
Quillzyy <shaquirearija1@gmail.com>
2023-10-10 12:15:40 +0700

    Adding more information to program

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -1,11 +1,14 @@
+# Using AWS IOT as MQTT Broker
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 
 MQTTclient = AWSIoTMQTTClient("LM-key")  # Random Key
 MQTTclient.configureEndpoint(
-    "a2pr7kvz6d0wr2-ats.iot.ap-southeast-2.amazonaws.com", 8883
+    "a2pr7kvz6d0wr2-ats.iot.ap-southeast-2.amazonaws.com",
+    8883,
 )
 
+# Certificate with RootCA, Private Key, and Certificate
 MQTTclient.configureCredentials(
     "./.cert/AmazonRootCA1.crt",
     "./.cert/private.pem.key",
@@ -20,6 +23,8 @@ MQTTclient.configureMQTTOperationTimeout(5)  # 5 sec
 print("Connecting to AWS IoT Broker...")
 MQTTclient.connect()
 print("Publishing message to AWS IoT Broker...")
+
+# Set topic to 'awsiot/test' and publish a message
 MQTTclient.publish(
     topic="awsiot/test",
     QoS=1,
--- mqtt/subscribe.py
+++ mqtt/subscribe.py
@@ -1,7 +1,9 @@
+# Using AWS IOT as MQTT Broker
 import time
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 
+# Callback function to print message received from AWS IoT Core
 def callback(self, params, packet):
     print("Received message from AWS IoT Core")
     print("Topic: " + packet.topic)
@@ -10,9 +12,11 @@ def callback(self, params, packet):
 
 MQTTclient = AWSIoTMQTTClient("LM-key")  # Random Key
 MQTTclient.configureEndpoint(
-    "a2pr7kvz6d0wr2-ats.iot.ap-southeast-2.amazonaws.com", 8883
+    "a2pr7kvz6d0wr2-ats.iot.ap-southeast-2.amazonaws.com",
+    8883,
 )
 
+# Certificate with RootCA, Private Key, and Certificate
 MQTTclient.configureCredentials(
     "./.cert/AmazonRootCA1.crt",
     "./.cert/private.pem.key",
@@ -24,10 +28,16 @@ MQTTclient.configureDrainingFrequency(2)  # Draining: 2 Hz
 MQTTclient.configureConnectDisconnectTimeout(10)  # 10 sec
 MQTTclient.configureMQTTOperationTimeout(5)  # 5 sec
 
+# Set topic to 'awsiot/test' and subscribe to a message
 print("Connecting to AWS IoT Broker...")
 MQTTclient.connect()
-MQTTclient.subscribe("awsiot/test", 1, callback)
+MQTTclient.subscribe(
+    "awsiot/test",
+    1,
+    callback,
+)
 print("Waiting for messages...")
 
+# Wait for messages by looping infinitely
 while True:
     time.sleep(5)
