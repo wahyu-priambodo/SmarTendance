d8b37e862e3b94f2ada830a507197175d248af38
Wahyu Priambodo <78311798+wahyu-priambodo@users.noreply.github.com>
2023-10-23 22:12:32 +0700

    Coba 2 by Wahyu (Diarahkan ke dashboard setelah user berhasil login)

--- app.py
+++ app.py
@@ -1,38 +1,52 @@
 from flask import Flask, request, render_template
 
 class AttendanceSystem:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.configure_routes()
-    
-    def configure_routes(self):
-        self.app.add_url_rule('/', view_func=self.index, methods=['GET', 'POST'])
-        self.app.add_url_rule('/dosen', view_func=self.login_dosen)
-        self.app.add_url_rule('/tutorial', view_func=self.tutorial)
+  # Daftar user
+  users = [
+    {
+      'Nama': 'Wahyu Priambodo',
+      'NIM': '2207421048',
+      'Password': '123456'
+    },
+    {
+      'Nama': 'Muhammad Khairu Mufid',
+      'NIM': '2207421039',
+      'Password': '12345678'
+    }
+  ]
 
-    def index(self):
-        nim = None
-        password = None
-        if request.method == 'POST':
-            nim = request.form.get('NIM')
-            password = request.form.get('password')
-            if nim == '2207421048' and password == '123456':
-                # NIM dan kata sandi cocok, arahkan ke halaman dashboard
-                return render_template('Dashboard_mahasiswa.html', nim=nim)
-            else:
-                # NIM dan kata sandi tidak cocok, tampilkan pesan kesalahan
-                return render_template('Portal_utama.html', error_message='NIM atau kata sandi salah')
-        return render_template('Portal_utama.html', nim=nim, password=password)
+  def __init__(self):
+    self.app = Flask(__name__)
+    self.configure_routes()
 
-    def login_dosen(self):
-        return render_template('Login_dsn.html')
+  def configure_routes(self):
+    self.app.add_url_rule('/', view_func=self.index)
+    self.app.add_url_rule('/dosen', view_func=self.login_dosen)
+    self.app.add_url_rule('/tutorial', view_func=self.tutorial)
+    self.app.add_url_rule('/dashboard', view_func=self.dashboard, methods=['GET'])
+
+  def index(self):
+    return render_template('Portal_utama.html')
+
+  def login_dosen(self):
+    return render_template('Login_dsn.html')
+
+  def dashboard(self):
+    nim = request.args.get('NIM')
+    password = request.args.get('Password')
     
-    def tutorial(self):
-        return render_template('Tutorial.html')
-    
-    def run(self):
-        self.app.run(debug=True)
+    for user in self.users:
+      if user['NIM'] == nim and user['Password'] == password:
+        return render_template('Dashboard.html', nama=user['Nama'])
+    else:
+      return "NIM atau Password salah"
+
+  def tutorial(self):
+    return render_template('Tutorial.html')
+
+  def run(self):
+    self.app.run(debug=True)
 
 if __name__ == '__main__':
-    app = AttendanceSystem()
-    app.run()
+  app = AttendanceSystem()
+  app.run()
